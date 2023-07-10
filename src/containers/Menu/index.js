81fcb235bde343c9821a87fe57d16f671d4db7a2
nogayegueye <nogayegye@gmail.com>
2023-07-10 02:10:06 +0200

    Modification de la page index.js dans le répertoire containers/menu afin d'assurer le bon fonctionnement du bouton Contact en redirigeant de manière appropriée vers le formulaire de contact

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -1,27 +1,33 @@
-/* eslint-disable no-return-assign */
+import React from 'react';
 import Button from "../../components/Button";
 import Logo from "../../components/Logo";
 
 import "./style.scss";
 
-const Menu = () => (
-  <nav>
-    <Logo />
-    <ul>
-      <li>
-        <a href="#nos-services">Nos services</a>
-      </li>
-      <li>
-        <a href="#nos-realisations">Nos réalisations</a>
-      </li>
-      <li>
-        <a href="#notre-equipe">Notre équipe</a>
-      </li>
-    </ul>
-    <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
-      Contact
-    </Button>
-  </nav>
-);
+const Menu = () => {
+  const handleContactClick = () => {
+    window.location.href = "#contact";
+  };
+
+  return (
+    <nav>
+      <Logo />
+      <ul>
+        <li>
+          <a href="#nos-services">Nos services</a>
+        </li>
+        <li>
+          <a href="#nos-realisations">Nos réalisations</a>
+        </li>
+        <li>
+          <a href="#notre-equipe">Notre équipe</a>
+        </li>
+      </ul>
+      <Button title="contact" onClick={handleContactClick}>
+        Contact
+      </Button>
+    </nav>
+  );
+};
 
 export default Menu;
